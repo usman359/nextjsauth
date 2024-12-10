7edf4cc362b22b6d84cf77e048058a3c110cda4d
usman359 <m.usman095@gmail.com>
2024-12-10 09:52:55 +0500

    commited

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
-interface Params {
-  params: {
-    id: string;
-  };
-}
-
-export default function UserProfile({ params }: Params) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function UserProfile({ params }: any) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
